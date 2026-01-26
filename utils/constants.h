525513edc94e6879600de4fa37dbc0827c9e682c
Pruden <prudencosta@gmail.com>
2026-01-26 16:27:11 +0100

    Fichero constantes

---
+++ utils/constants.h
@@ -0,0 +1,12 @@
+//
+// Created by pruden on 26/1/26.
+//
+
+#ifndef TERMINAL_BREAKOUT_CONSTANTS_H
+#define TERMINAL_BREAKOUT_CONSTANTS_H
+
+constexpr int WIDTH = 20;
+constexpr int HEIGHT = 40;
+
+
+#endif //TERMINAL_BREAKOUT_CONSTANTS_H

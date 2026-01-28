afd8bc7f9b2a8362a8bfbf6f521bc8b53c797d91
Pruden <prudencosta@gmail.com>
2026-01-28 07:31:53 +0100

    Fix esc controles

--- keyboard/KeyboardManager.cpp
+++ keyboard/KeyboardManager.cpp
@@ -42,26 +42,30 @@ int read_last_direction(Player& player, Board& board, Ball& ball) {
         timeout.tv_sec = 0;
         timeout.tv_usec = 0;
 
-        if (select(STDIN_FILENO + 1, &read_fds, nullptr, nullptr, &timeout) <= 0) {
+        if (select(STDIN_FILENO + 1, &read_fds, nullptr, nullptr, &timeout) <= 0)
             break;
-        }
 
         char c = getchar();
 
         if (c == '\033') {
-            getchar();
-            c = getchar();
+            char c1 = getchar();
+            if (c1 != '[') {
+                restore_terminal();
+                exit(0);
+            }
 
-            if (c == 'C') last_dir = DIR_RIGHT;
-            else if (c == 'D') last_dir = DIR_LEFT;
+            char c2 = getchar();
+            if (c2 == 'C') last_dir = DIR_RIGHT;
+            else if (c2 == 'D') last_dir = DIR_LEFT;
+
         } else if (c == 'q') {
             restore_terminal();
             exit(0);
+
         } else if (c == 'r') {
             player.set_game_over(false);
             player.set_lives(3);
             player.restart_pos();
-
             board.init();
             ball.ball_reset(board);
         }
